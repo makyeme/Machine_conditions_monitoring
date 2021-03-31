62198470b3706ef9cf53c70dcd93ff6a89352d61
simonsny <snyders.sim@gmail.com>
2021-03-31 19:01:00 +0200

    big commit notebooks

--- functions/functions.py
+++ functions/functions.py
@@ -4,11 +4,11 @@ from scipy.stats import skew
 from scipy.stats import entropy
 
 
-
 def abs_fft(wave: np.array):
     return np.abs(np.fft.fft(wave))
 
-def simple_stats(x, key, feature=True, entropy=True, extra_info={}):
+
+def simple_stats(x: np.array, key: str, extra_info: dict) -> dict:
     data = {}
 
     if key == 'fft':
@@ -19,29 +19,32 @@ def simple_stats(x, key, feature=True, entropy=True, extra_info={}):
             data[f'{key}_{hz_range}_mean'] = np.mean(
                 x[hz_range[0]:hz_range[1]]) / n
 
-
-    data[key + f'{key}_mean'] = np.mean(x)
+    data[f'{key}_mean'] = np.mean(x)
     data[f'{key}_median'] = np.median(x)
     data[f'{key}_std'] = np.std(x)
-    print(type(kurtosis(x)))
-    data[f'{key}_kurtosis'] = kurtosis(x)
-    data[f'{key}_skew'] = skew(x)
     data[f'{key}_min'] = np.min(x)
     data[f'{key}_max'] = np.max(x)
     data[f'{key}_amp'] = data[f'{key}_max'] - data[f'{key}_min']
-    data[f'{key}_SNR'] = data[f'{key}_median'] / data[f'{key}_std']
-    if feature:
-        data[f'{key}_entropy'] = entropy(x, axis=1)[0]
-        data[f'{key}_kurtosis'] = kurtosis(x, axis=1)[0]
-        data[f'{key}_skew'] = skew(x, axis=1)[0]
+    data[f'{key}_SNR'] = data[f'{key}_mean'] / data[f'{key}_std']
+    if key == 'fft':
+        data[f'{key}_entropy'] = entropy(x)
+        data[f'{key}_kurtosis'] = kurtosis(x)
+        data[f'{key}_skew'] = skew(x)
+        return data
+    if x.ndim == 2:
+        if key != 'mfcc':
+            data[f'{key}_entropy'] = entropy(x, axis=1).mean()
+        data[f'{key}_kurtosis'] = kurtosis(x, axis=1).mean()
+        data[f'{key}_skew'] = skew(x, axis=1).mean()
+        mean_x = np.mean(x, axis=1)
+        std_x = np.std(x, axis=1)
+        for i in range(len(mean_x)):
+            data[f'{key}_mean_{i}'] = mean_x[i]
+            data[f'{key}_std_{i}'] = std_x[i]
+
     else:
-        print('x', x)
-        print('data', data)
-        print('key', key)
-        data[f'{key}_entropy'] = entropy(x, axis=1)
-        data[f'{key}_kurtosis'] = kurtosis(x, axis=1)
-        data[f'{key}_skew'] = skew(x, axis=1)
-    ## Percentiles
-    for number, percent in zip(np.percentile(x, extra_info['percentiles']), extra_info['percentiles']):
-        data[f'{key}_percentile_{percent}'] = number
+        data[f'{key}_kurtosis'] = kurtosis(x)
+        data[f'{key}_skew'] = skew(x)
+        for number, percent in zip(np.percentile(x, extra_info['percentiles']), extra_info['percentiles']):
+            data[f'{key}_percentile_{percent}'] = number
     return data
--- functions/preprocessing.py
+++ functions/preprocessing.py
@@ -1,21 +1,26 @@
 import pandas as pd
-import numpy as np
 import librosa
-import IPython.display as ipd #palying audio
-import matplotlib.pyplot as plt
 import librosa.display
-import csv
 import glob
 from functions.functions import abs_fft
 from functions.functions import simple_stats
+from time import time
 
 
 import os
 
 SR = 16000
 PERCENTILES = [10*i for i in range(1, 10)]
-FREQUENCY_RANGES = [(0, 5), (5, 20), (20, 50), (50, 200), (200, 500), (500, 2000), (2000, 5000), (5000, 20000),
-                             (20000, 50000), (50000, 160000)]
+FREQUENCY_RANGES = [(0, 5),
+                    (5, 20),
+                    (20, 50),
+                    (50, 200),
+                    (200, 500),
+                    (500, 2000),
+                    (2000, 5000),
+                    (5000, 20000),
+                    (20000, 50000),
+                    (50000, 160000)]
 
 
 class Features:
@@ -24,6 +29,7 @@ class Features:
     """
 
     def __init__(self, feature_functions=None, sr=16000):
+        print('lolo')
         self.sampling_rate = sr
         if isinstance(feature_functions, type(None)):
             self.feature_functions = {
@@ -37,43 +43,56 @@ class Features:
                 'fft': abs_fft,
             }
 
-    def extract_feature(self, path):
+    def extract_features(self, path):
         wave, sr = librosa.load(path, sr=self.sampling_rate)
-
+        extra_info = {'frequency_ranges': FREQUENCY_RANGES, 'percentiles': PERCENTILES}
         data = {}
-        percentiles = [10 * i for i in range(1, 10)]
-        frequency_ranges = [(0, 5), (5, 20), (20, 50), (50, 200), (200, 500), (500, 2000), (2000, 5000), (5000, 8000)]
-
         # Some standard statistics you can take from an array, a lot of duplicate code that can be removed, made better
         # percentiles also contain the median (and could contain the min and max as well but not currently)
 
         # Statistics on the wave itself
         data.update(simple_stats(x=wave,
                                  key='wave',
-                                 feature=False,
-                                 entropy=False,
-                                 extra_info=FREQUENCY_RANGES))
+                                 extra_info=extra_info))
 
         for key, function in self.feature_functions.items():
             if key == 'fft':
                 transformed_wave = function(wave)
-                data.update(simple_stats(transformed_wave, key='key', feature=False, entropy=False))
+                data.update(simple_stats(transformed_wave, key=key, extra_info=extra_info))
                 continue
             try:
                 transformed_wave = function(y=wave, sr=self.sampling_rate)
             except TypeError:
                 transformed_wave = function(y=wave)
 
-
-                    # Ugly if statements, could be done better
-            if transformed_wave.ndim < 2:
-                data.update(simple_stats(transformed_wave, key='key', feature=True))
-
-            else:
-                mtw = np.mean(transformed_wave, axis=1)
-                for i, row in enumerate(mtw):
-                    data[f'{key}_mean_{i}'] = row
+            data.update(simple_stats(transformed_wave, key=key, extra_info=extra_info))
 
         data['path'] = path
         return data
 
+    def extract_features_from_machine(self, machine, save=True):
+        list_paths_machine = glob.glob(f'data/*/{machine}/*/*/*.wav')
+        length = len(list_paths_machine)
+        print(f'files: {length}')
+        t0 = time()
+        data_list = []
+        for i, path in enumerate(list_paths_machine):
+            if i % 200 == 1:
+                p_done = i / length * 100
+                print(f'****************** {machine} ******************')
+                print(f'****************** {machine} ******************')
+                print(f'****************** {machine} ******************')
+                print("{:.2f}%".format(p_done))
+                t1 = time()
+                td = t1 - t0
+                time_left = td / i * (length - i)
+                print(f'Estimated time left: {int(time_left / 60 * 100) / 100} minutes')
+            data_list.append(self.extract_features(path))
+        df = pd.DataFrame(data_list)
+        if save:
+            if not os.path.exists('data_features'):
+                os.makedirs('data_features')
+            df.to_csv(f'data_features/features_{machine}.csv')
+        return df
+
+    
